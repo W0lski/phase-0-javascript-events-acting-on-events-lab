d9ef931c6318d3c25a07dfc9c1302a257881d65e
Wol <woldut101@icloud.com>
2024-04-09 12:20:01 +0300

    Adds Phase-0 JavaScript Events Acting on Events Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+// Function to move the dodger to the left
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  // Function to move the dodger to the right
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
